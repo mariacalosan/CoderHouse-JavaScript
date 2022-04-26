acc23f8309c09329d8b35fc8f0150b552d155d09
Maria Camila Lopez <mariacalosan@gmail.com>
2022-04-26 17:22:24 -0500

    Haciendo condicional ejercicio 

---
+++ condicionales.js
@@ -0,0 +1,24 @@
+/*let numero= prompt("ingrese un numero");
+
+if (numero > 1000){
+  console.log("el numero es mayor a 1000")
+}*/
+
+
+//Pedir un texto mediante prompt, y si es igual a "Hola" mostrar un alerta por consola.
+
+/*let texto= prompt("escribe un palabra");
+
+let palabra= "Hola";
+
+if (texto == palabra){
+  console.log("Hola mundo");
+}*/
+
+let numero= prompt("ingrese un numero");
+
+if ((numero >= 10) && (numero <=50)){
+  console.log("el numero es mayor que 10 y menor que 50")
+}
+
+
