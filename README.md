590d7a82f38b79002ba36df795bd1835c4570748
maria camila <mariacalosan@gmail.com>
2022-05-10 08:07:29 -0500

    ejercicios durante la clase

---
+++ README.md
@@ -0,0 +1,8 @@
+## tipo de datos que recibe jS
+
+<img src="https://static.platzi.com/media/user_upload/CU01112E_1-f9d2b6fc-f60c-4bf4-a61d-6bf9df36b268.jpg">
+
+## Variables
+
+¿Que es una variable?
+las variables sirven para guardar valores en memeoria
---
+++ funciones.js
@@ -0,0 +1,11 @@
+/* 1) Crear una función declarada que de la bienvenida al programa "Inicializando programa...."
+* 2) Crear una función declarada que pida el nombre y que pida un genero y en base al genero salude como Bienvenido / Bienvenida / Bievenide + el nombre 
+* 3) Crear una función anonima que tenga un parametro que muestre el siguiente de un numero
+* 4) Crear una funcion anonima que muestre el siguiente y el anterior de un numero
+* 5) Tomar algunas de las funciones que ya hicieron y convertirla en flecha 
+*/
+
+function number(onenumber, twonumber){
+  console.log("onenumber, twonumber");
+}
+number (2,3);
