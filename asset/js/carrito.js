2ca44ce47bb481938e014d9f822d7f87cad70c2c
maria camila <mariacalosan@gmail.com>
2022-06-14 19:51:57 -0500

    haciendo  un ejemplo del registro de usuario

--- asset/js/carrito.js
+++ asset/js/carrito.js
@@ -1,3 +1,18 @@
+//Clase carrito   
+class Carrito {
+    constructor() {
+        this.lista = [];
+    }
+    agregar(item) {
+        this.lista.push(item);
+        mostrarCarrito();
+    }
+    eliminar(numero) {
+        this.lista.splice(numero, 1);
+        mostrarCarrito();
+    }
+}
+
 //funciones     
 function functionAgregar(numero) {
     for (let i = 0; i < articulos.length; i++) {
@@ -12,6 +27,7 @@ function total() {
     }
     let nuevot = getID('total');
     nuevot.innerText = total + "$";
+    sessionStorage.setItem("carro", JSON.stringify(carro.lista));
     let nuevoc = getID('conteo')
     let contado = 0;
     nuevoc.innerText = carro.lista.length;
@@ -20,7 +36,7 @@ function total() {
 function mostrarCarrito() {
     almacen.innerHTML = null;
     for (let i = 0; i < carro.lista.length; i++) {
-        almacen.innerHTML += `<div><img src=${carro.lista[i].img}><p>${carro.lista[i].nombre}</p><span>${carro.lista[i].precio + "$"}
+        almacen.innerHTML += `<div class="asaide-articulos"><img src=${carro.lista[i].img1}><p>${carro.lista[i].nombre}</p><span>${carro.lista[i].precio + "$"}
         <i class="fa-solid fa-trash-arrow-up deleteA " id="deleteA-${i}"></i></span></div>\n`;
     }
     total();
@@ -40,14 +56,12 @@ almacen.addEventListener('click', (e) => {
     }
 })
 
-
-
-
-/*Incluir fundamentalmente en la segunda pre-entrega:
-
-Eventos
-DOM
-Funciones
-Arreglos
-Objetos
-Storage (sessionStorage y/o localStorage)*/
+// sessionStorage
+if (!!sessionStorage.getItem("carro")) {
+    let guardado = JSON.parse(sessionStorage.getItem("carro"));
+    if (guardado.length > 0) {
+        carro.lista = guardado;
+        mostrarCarrito();
+        clickCarrito();
+    }
+}
